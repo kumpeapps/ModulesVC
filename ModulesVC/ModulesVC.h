9599ca6ff9f3699a77f70acf6eee83483483137a
Justin Kumpe <jakumpe@kumpes.com>
2022-07-26 17:57:57 -0500

    Initial Commit

---
+++ ModulesVC/ModulesVC.h
@@ -0,0 +1,18 @@
+//
+//  ModulesVC.h
+//  ModulesVC
+//
+//  Created by Justin Kumpe on 7/26/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModulesVC.
+FOUNDATION_EXPORT double ModulesVCVersionNumber;
+
+//! Project version string for ModulesVC.
+FOUNDATION_EXPORT const unsigned char ModulesVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModulesVC/PublicHeader.h>
+
+
